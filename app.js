0ed86d25646f5d100eeb568ab6880e97e77e9f06
Alvin Cousins <alvin.cousins@googlemail.com>
2021-05-06 15:05:55 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// Middleware: adding data to request object.
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to the endpoint...');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// Route for 'id' variable in url
+// ? after param defines optional param
+app.get('/api/v1/tours/:id', (req, res) => {
+  // req.params give access to the assigned id value/ variable.
+  console.log(req.params);
+
+  // Find takes a callback function, which loops through array, returning true of false.
+  // Find method creates a new array for each value in array that is 'true'.
+  const id = req.params.id * 1; // Converts string to number.
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
