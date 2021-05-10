b630b9104c7cd29adaefaedb8cea9977997ffec4
Alvin Cousins <alvin.cousins@googlemail.com>
2021-05-10 15:08:05 +0100

    Query filtering and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,31 +1,110 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
 };
 
-exports.getTour = (req, res) => {
-  // req.params give access to the assigned id value/ variable.
-  console.log(req.params);
-  const id = req.params.id * 1; // '* 1' Converts string to number.
-  // Find takes a callback function, which loops through array, returning true of false.
-  // Find method creates a new array for each value in array that is 'true'.
-  // const tour = tours.find((el) => el.id === id);
+exports.getAllTours = async (req, res) => {
+  try {
+    console.log(req.query);
+    // BUILD QUERY
+    // 1a) Filtering
+    // Shallow copy of query object:
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // Remove exluded fields from query objects.
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // Compare queryObj with amended/ queryObj copy.
+    // console.log(req.query, queryObj);
 
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+    // Access query string: req.query. Returns query object, which can be passed into find() method.
+    // console.log(req.query);
+
+    // 1b) Advanced filtering
+    // Convert object to string.
+    // Mongo uses "$" in operator. Need to find gte/lte operators in query string and replace with $gte/$lte etc.
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // Find method on the Tour model.
+    // find() method returns everything if nothing passed in.
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    // Sorting criteria sepatated by comma in URL. Need to replace with space.
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      // Default sort
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      // Exclude field "-"
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    // "|| 1": default value
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // 1-10, page; 11-20, page 2...
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      // Check if number of documents does not exceed page number.
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTour) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    // ":id" is passed as a param through route.
+    // findById() is the same as MongoDB Tour.findOne({ _id: req.params.id})
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // Async function to communicate with database.
@@ -53,20 +132,42 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    // findByIdAndUpdate(<id>, <data to change>, <options> )
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      // return modified doc instead of original
+      new: true,
+      // validate operation against model's schema. checks data types (string number etc), required etc...
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 // "exports." instead of const or "module.exports = router;" to export individual/ multiple functions.
--- models/tourModel.js
+++ models/tourModel.js
@@ -7,15 +7,53 @@ const tourSchema = new mongoose.Schema({
     // required: true + error string
     required: [true, 'A tour must have a name.'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price.'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String, // Reference to image in database
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String], // Array of Strings.
+  createdAt: {
+    type: Date,
+    default: Date.now(), // Converted to today's date by Mongo
+    select: false, // Hides field from output
+  },
+  startDates: [Date], // Array of start dates
 });
 // Create new model out of schema defined above.
 const Tour = mongoose.model('Tour', tourSchema);
