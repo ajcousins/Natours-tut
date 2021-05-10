9515a7dc96691859288dba18a63806c5f98fb6db
Alvin Cousins <alvin.cousins@googlemail.com>
2021-05-10 15:39:30 +0100

    Refactoring API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,13 +8,16 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    console.log(req.query);
-    // BUILD QUERY
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
     // 1a) Filtering
     // Shallow copy of query object:
-    const queryObj = { ...req.query };
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     // Remove exluded fields from query objects.
     excludedFields.forEach((el) => delete queryObj[el]);
@@ -29,47 +32,70 @@ exports.getAllTours = async (req, res) => {
     // Mongo uses "$" in operator. Need to find gte/lte operators in query string and replace with $gte/$lte etc.
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    // console.log(JSON.parse(queryStr));
 
     // Find method on the Tour model.
     // find() method returns everything if nothing passed in.
-    let query = Tour.find(JSON.parse(queryStr));
 
+    this.query = this.query.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
     // 2) Sorting
     // Sorting criteria sepatated by comma in URL. Need to replace with space.
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
     } else {
       // Default sort
-      query = query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
+    return this;
+  }
 
+  limitFields() {
     // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
       // Exclude field "-"
-      query = query.select('-__v');
+      this.query = this.query.select('-__v');
     }
+    return this;
+  }
 
+  paginate() {
     // 4) Pagination
     // "|| 1": default value
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
     // 1-10, page; 11-20, page 2...
-    query = query.skip(skip).limit(limit);
+    this.query = this.query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      // Check if number of documents does not exceed page number.
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTour) throw new Error('This page does not exist');
-    }
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    console.log(req.query);
+    // BUILD QUERY
 
     // EXECUTE QUERY
-    const tours = await query;
+    // Create new object of the APIFeatures class.
+    // In there we pass a query object and query string.
+    // In each method, the query is manipulated.
+    // Then await result of query, which is stored in 'const features'.
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
